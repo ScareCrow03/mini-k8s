809432f07a1a67c1bf3fb06a4959cd24a0075268
ILoLy10 <iloly10@sjtu.edu.cn>
2024-05-30 01:22:45 -0700

    small fix

--- script/install_nfs_master.sh
+++ script/install_nfs_master.sh
@@ -10,7 +10,7 @@ sudo apt-get install nfs-server
 # echo "welcome to NFS">test
 
 mkdir /srv/mini-k8s
-sudo chmod 777 /srv/mini-k8s/*
+sudo chmod 777 /srv/mini-k8s/
 sudo echo "welcome to mini-k8s NFS" > test
 # sudo bash -c "echo \"welcome to mini-k8s NFS\" > test"
 
@@ -19,8 +19,9 @@ sudo systemctl restart rpcbind
 
 # cat /etc/exports
 # 注意exports里面(rw,sync,no_root_squash)不能加空格
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-sudo cp /home/zyc/Desktop/mini-k8s/assets/test_persistent/exports /etc/
+sudo cp ${SCRIPT_DIR}/../assets/test_persistent/exports /etc/
 
 sudo /etc/init.d/nfs-kernel-server restart
 
--- script/install_nfs_worker.sh
+++ script/install_nfs_worker.sh
@@ -1,8 +1,10 @@
 # 先执行 chmod 777 ./script/install_nfs_worker.sh
+# 记得改为主机ip
 
 sudo apt-get install rpcbind
 
 mkdir /srv/mini-k8s
+sudo chmod 777 /srv/mini-k8s/
 
 sudo echo "192.168.172.128:/srv/mini-k8s /srv/mini-k8s nfs defaults 0 0" >> /etc/fstab
 
